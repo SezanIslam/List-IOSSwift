b4c4907c8c7e9e3962b412cfe9a4d2ec730eae7a
sezanislam <sezan.islam@rutgers.edu>
2022-11-26 12:49:04 -0500

    Initial Commit

---
+++ ToDoList/Controller/ToDoCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+@objc protocol ToDoCellDelegate: class {
+    func checkmarkTapped(sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func completeButtonTapped() {
+        delegate?.checkmarkTapped(sender: self)
+    }
+
+}
